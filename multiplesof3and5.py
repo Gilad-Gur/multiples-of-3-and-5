f10aef8e6407eaf7377cef7ee37786fa164ee9ba
Gilad Gur <giladgur0001@gmail.com>
2017-12-17 14:07:11 +0300

    Update multiplesof3and5.py

--- multiplesof3and5.py
+++ multiplesof3and5.py
@@ -1,22 +1,41 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5,
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # This program finds the sum of all the multiples of 3 or 5 below a given positive integer.
-while True:
-    try:
-        n = int(input('Please type in a natural number: '))
-    except ValueError:
-        print('Invalid input')
-        continue
-    if n <= 0:
-        print('Invalid input')
-        continue
-    break
+def user_input():
+    while True:
+        try:
+            n = input('Please type in a natural number: ')
+            if (n=='q'):
+                break
+            else:
+                n=int(n)
+            break
+        except ValueError:
+            print('Invalid input')
+            continue
+        if n <0:
+            print('Invalid input')
+            continue
 
-sum = 0
-print("List of all the natural numbers below " +str(n)+ " that are mult. of 3 & 5:")
+    return n
 
-for i in range(n):
-    if i%3==0 or i%5==0:
-        print(i)
-        sum += i
-print("The sum of these multiples is " +str(sum))
+
+
+def sumDivisibleBy(div,n):
+    p = (n-1) // div
+    sum = (div*p*(p+1))//2
+    return sum
+
+
+
+def main():
+    while True:
+        n=user_input()
+        if (n=='q'):
+            break
+        print(sumDivisibleBy(5,n)+sumDivisibleBy(3,n)-sumDivisibleBy(15,n))
+        print('do it again!!!')
+
+
+
+main()
